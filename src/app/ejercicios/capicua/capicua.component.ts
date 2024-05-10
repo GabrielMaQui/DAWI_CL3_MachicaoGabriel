5fddabd13517213a3807aa2bf03df6f1e388c9aa
Gabriel <gmachicaoq@unsa.edu.pe>
2024-05-10 14:36:43 -0500

    Se completa el ejercicio y se coloca validaciones

--- src/app/ejercicios/capicua/capicua.component.ts
+++ src/app/ejercicios/capicua/capicua.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-capicua',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './capicua.component.html',
   styleUrl: './capicua.component.scss'
 })
 export class CapicuaComponent {
+
+  capicua = {
+    numero: 0,
+    resultado: '',
+    error: 'Ingrese un número de dos cifras y positivo'
+  };
+  mostrarMensaje = false;
+  mostrarError = false;
+
+  verificarCapicua() {
+    if (this.capicua.numero > 9 && this.capicua.numero <= 99){
+      this.mostrarMensaje = true;
+      this.mostrarError = false;
+      let numeroString = this.capicua.numero.toString();
+      let numeroInvertido = numeroString.split('').reverse().join('');
+      if(numeroString === numeroInvertido){
+        this.capicua.resultado = "El número " + this.capicua.numero + " es capicúa";
+      }
+      else{
+        this.capicua.resultado = "El número " + this.capicua.numero + " no es capicúa";
+      }
+    }
+    else{
+      this.mostrarMensaje = false;
+      this.mostrarError = true;
+    }
+  }
 
 }
