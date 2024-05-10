dcdf62b7b921a798399f6c48c4d3e48b3b3a368a
Gabriel <gmachicaoq@unsa.edu.pe>
2024-05-10 16:51:34 -0500

    Mejorando las validaciones del trabajo

--- src/app/ejercicios/edades/edades.component.ts
+++ src/app/ejercicios/edades/edades.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+
 @Component({
   selector: 'app-edades',
   standalone: true,
@@ -14,28 +15,41 @@ export class EdadesComponent {
     edad1: 0,
     edad2: 0,
     edadMayor: '',
-    diferencia: 0
+    diferencia: 0,
+    error: 'Rango de numeros equivocado, ingrese un numero entre 0 y 120 y que sea entero'
   }
 
   mostrarMensaje = false;
+  mostrarError = false;
 
   calcularDiferencia() {
-    this.edades.edad1 = Math.round(this.edades.edad1);
-    this.edades.edad2 = Math.round(this.edades.edad2);
-    this.mostrarMensaje = true;
-    if(this.edades.edad1 > this.edades.edad2){
-      this.edades.edadMayor = "La edad del mayor es: " + this.edades.edad1 + " años";
-      this.edades.diferencia = this.edades.edad1 - this.edades.edad2;
-    }
 
-    else if(this.edades.edad1 < this.edades.edad2){
-      this.edades.edadMayor = "La edad del mayor es: " + this.edades.edad2 + " años";
-      this.edades.diferencia = this.edades.edad2 - this.edades.edad1;
-    }
-    else{
-      this.edades.edadMayor = "No hay ninguna edad mayor, las edades son iguales";
-      this.edades.diferencia = 0;
-    }
+    if(this.edades.edad1 < 0 || this.edades.edad2 < 0 || this.edades.edad1 > 120 || this.edades.edad2 > 120
+        || Number.isInteger(this.edades.edad1) || Number.isInteger(this.edades.edad2)){
+
+        this.mostrarMensaje = true;
+        this.mostrarError = false;
+        if(this.edades.edad1 > this.edades.edad2){
+          this.edades.edadMayor = "La edad del mayor es: " + this.edades.edad1 + " años";
+          this.edades.diferencia = this.edades.edad1 - this.edades.edad2;
+        }
+
+        else if(this.edades.edad1 < this.edades.edad2){
+          this.edades.edadMayor = "La edad del mayor es: " + this.edades.edad2 + " años";
+          this.edades.diferencia = this.edades.edad2 - this.edades.edad1;
+        }
+        else{
+          this.edades.edadMayor = "No hay ninguna edad mayor, las edades son iguales";
+          this.edades.diferencia = 0;
+        }
+
+  }else {
+
+    this.mostrarMensaje = false;
+    this.mostrarError = true;
+
+
+  }
 
   }
 
--- src/app/ejercicios/telefono-minutos/telefono-minutos.component.ts
+++ src/app/ejercicios/telefono-minutos/telefono-minutos.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+
 @Component({
   selector: 'app-telefono-minutos',
   standalone: true,
@@ -15,20 +16,28 @@ export class TelefonoMinutosComponent {
   planCelular = {
     numero: 0,
     minutos: 0,
-    costo: 0
+    costo: 0,
+    error: 'Los minutos deben ser enteros y positivos'
   }
 
   mostrarMensaje = false;
+  mostrarError = false;
 
   calcularCosto() {
 
-    this.mostrarMensaje = true;
-    if(this.planCelular.minutos <= 5){
-      this.planCelular.costo = 10;
-    }
-    else{
-      this.planCelular.costo = 10 + (this.planCelular.minutos - 5) * 2;
-    }
+    if(this.planCelular.minutos > 0 && Number.isInteger(this.planCelular.minutos)){
+        this.mostrarMensaje = true;
+        this.mostrarError = false;
+        if(this.planCelular.minutos <= 5){
+          this.planCelular.costo = 10;
+        }
+        else{
+          this.planCelular.costo = 10 + (this.planCelular.minutos - 5) * 2;
+        }
+      }else{
+        this.mostrarMensaje = false;
+        this.mostrarError = true;
+      }
   }
 
 }
