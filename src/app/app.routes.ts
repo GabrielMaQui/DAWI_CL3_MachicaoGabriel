9b8764037b9d3dea0a8f4ea241b4531e59568fff
Gabriel <gmachicaoq@unsa.edu.pe>
2024-05-10 17:11:36 -0500

    Se hace el indice

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { TelefonoMinutosComponent } from './ejercicios/telefono-minutos/telefono
 import { EdadesComponent } from './ejercicios/edades/edades.component';
 import { CapicuaComponent } from './ejercicios/capicua/capicua.component';
 import { AsciiTableComponent } from './ejercicios/ascii-table/ascii-table.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 export const routes: Routes = [
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: '', component: InicioComponent },
   {path: 'telefono', component: TelefonoMinutosComponent},
   {path: 'edades', component: EdadesComponent},
   {path: 'capicua', component: CapicuaComponent},
   {path: 'tabla-ascii', component: AsciiTableComponent},
 ];
+
